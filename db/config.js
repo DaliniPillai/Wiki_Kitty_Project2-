822f7da93498c22c004fef1da3f8f06f23c0a9ad
Dalini <student@GAs-MacBook-Air.local>
2017-05-08 00:12:20 -0400

    fix server

--- db/config.js
+++ db/config.js
@@ -6,13 +6,10 @@ if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
   db = pgp({
     database: 'kitty_development',
     port: 5432,
-    host: '127.0.0.1'
+    host: 'localhost'
   });
 } else if (process.env.NODE_ENV === 'production') {
-  db = pgp({
-    database: 'kitty_production',
-    port: 5432,
-    host: '127.0.0.1'
-  });
+  db = pgp(process.env.DATABASE_URL);
 }
+
 module.exports = db;
