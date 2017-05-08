85a18697477e5c568ceabdf01f14302ec9564457
Dalini <student@GAs-MacBook-Air.local>
2017-05-07 23:57:34 -0400

    fix local host issue

--- db/config.js
+++ db/config.js
@@ -6,13 +6,13 @@ if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
   db = pgp({
     database: 'kitty_development',
     port: 5432,
-    host: 'localhost'
+    host: '127.0.0.1'
   });
 } else if (process.env.NODE_ENV === 'production') {
   db = pgp({
     database: 'kitty_production',
     port: 5432,
-    host: 'localhost'
+    host: '127.0.0.1'
   });
 }
 module.exports = db;
